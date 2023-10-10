1dc3a11497ed39e0603f7dbb049a83d0b1ecf7b6
lucianodiaconescu <luciano.diaconescu@gmail.com>
2023-10-10 09:00:28 +0300

    First Commit

---
+++ src/main/java/ro/uvt/info/splab/SplabApplication.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.splab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplabApplication.class, args);
+	}
+
+}
